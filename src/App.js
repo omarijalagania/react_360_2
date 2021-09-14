64b12b71a53f74d3271c0298a649ed9787764e27
omari <omari.jalagania@gmail.com>
2021-09-14 23:03:17 +0400

    autoload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactPannellum, { getConfig } from "react-pannellum";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
@@ -20,9 +19,12 @@ function App() {
   }
   const config = {
     autoRotate: -2,
-    pitch: 10
+    autoLoad: true,
+    
   };
   const classes = useStyles();
+
+  
 
   return (
     
